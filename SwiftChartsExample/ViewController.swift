2cf41691811d762a4a70e1d5645901eadd24e91e
Mike Litman <mumbl1@mac.com>
2015-12-22 09:02:13 -0600

    initial commit

--- SwiftChartsExample/ViewController.swift
+++ SwiftChartsExample/ViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import SwiftCharts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        let chartConfig = ChartConfigXY(
+            xAxisConfig: ChartAxisConfig(from: 1, to: 7, by: 1),
+            yAxisConfig: ChartAxisConfig(from: 0, to: 450, by: 2)
+        )
+        
+        let chart = LineChart(
+            frame: CGRectMake(0, 70, 300, 500),
+            chartConfig: chartConfig,
+            xTitle: "Days",
+            yTitle: "Blood Sugar",
+            lines: [
+                (chartPoints: [(1.0, 60), (2, 240), (3, 100), (4, 120), (5, 160), (6, 115), (7, 300)], color: UIColor.redColor())
+            ]
+        )
+        self.view.addSubview(chart.view)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
